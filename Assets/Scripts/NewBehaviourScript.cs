f333977f192ca390953f7dd0b8be25f9d40b96b0
Pako <Pak0on@mail.ru>
2023-09-22 18:07:09 +0300

    Commit 6
    Home work

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MyClass
+{
+    public int i;
+    public int j;
+}
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    void Start()
+    {
+        short s = 0;
+        int i = 0;
+        long l = long.MaxValue;
+        float f = 0;
+        double d = 0;
+
+        object o = d;
+        double d2 = (double)o;
+
+        i = (int)l;
+        l = i;
+
+        TestFunc(out i);
+        Debug.Log(i);
+
+        MyClass mc = new MyClass();
+
+        var gf = mc;
+
+        Debug.Log($"short: {short.MinValue} - {short.MaxValue}");
+        Debug.Log($"int: {int.MinValue} - {int.MaxValue}");
+        Debug.Log($"long: {long.MinValue} - {long.MaxValue}");
+        Debug.Log($"float: {float.MinValue} - {float.MaxValue}");
+        Debug.Log($"double: {double.MinValue} - {double.MaxValue}");
+    }
+
+    public void TestFunc(out int i)
+    {
+        i = 10;
+    }
+
+    public void TestFunc(MyClass mc)
+    {
+
+    }
+
+}
+
+
+public class MyList
+{
+
+    private int[] m_array = new int[4];
+
+    public int count {get; }
+    public void Push()
+    {
+
+    }
+
+    public void Insert(int item)
+    {
+
+    }
+
+    public int IndexOf(int item)
+    {
+        return 0;
+    }
+
+    public void Remove(int item)
+    {
+
+    }
+
+    public void RemoveAt(int index)
+    {
+
+    }
+
+    public void Clear()
+    {
+
+    }
+}
